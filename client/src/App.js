7042c87d701d45f2abd7555143cdf031a8328011
arcleah <arcciepasc@gmail.com>
2024-11-24 21:38:25 -0700

    set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
