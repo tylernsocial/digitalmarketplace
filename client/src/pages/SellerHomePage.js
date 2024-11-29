aadf3bd22967b488058b2782a2d5ec7214c7c708
arcleah <arcciepasc@gmail.com>
2024-11-29 00:23:20 -0700

    few seller-home-page changes

--- client/src/pages/SellerHomePage.js
+++ client/src/pages/SellerHomePage.js
@@ -1,8 +1,38 @@
 import React, { useState } from "react";
-
+import './SellerHomePage.css'; 
 
 export const SellerHomePage = () => {
+        // State to manage search query
+        const [searchQuery, setSearchQuery] = useState("");
+  
+        // Handle the change in input field
+        const handleSearchChange = (e) => {
+          setSearchQuery(e.target.value);
+        };
+      
+        // Handle form submission or search button click
+        const handleSearch = (e) => {
+          e.preventDefault();
+          // Logic for search (for now, just log the query)
+          console.log("Searching for:", searchQuery);
+        };
   return (
-    <div>SellerHomePage</div>
+    <div className="sellerpage-container">
+      <h1 className="sellerpage-title">Welcome to the Seller's portal!</h1>
+             {/* Search section */}
+      <div className="sellerpage-search">
+        <form onSubmit={handleSearch} style={{ width: "100%" }}>
+          <input
+            type="text"
+            value={searchQuery}
+            onChange={handleSearchChange}
+            placeholder="Search..."
+          />
+        </form>
+      </div>
+        
+    
+    
+    </div>
   )
 }
